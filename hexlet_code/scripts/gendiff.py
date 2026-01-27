b72ad0a429317685ed1567729895051f9f231e95
Ivan63-coder <vanyaiudin@yandex.ru>
2026-01-27 14:43:07 +0400

    Add script, entry_point, gendiff

---
+++ hexlet_code/scripts/gendiff.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
+    )
+
+    parser.add_argument(
+        'first_file',
+    )
+
+    parser.add_argument(
+        'second_file',
+    )
+
+    args = parser.parse_args()
+
+    print(f"Comparing {args.first_file} and {args.second_file}")
+    print("Difference will be shown here...")
+
+
+if __name__ == '__main__':
+    main()
