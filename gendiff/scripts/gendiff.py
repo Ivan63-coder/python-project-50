43725f2792770d78a864a94cd04040107eec7550
Ivan63-coder <vanyaiudin@yandex.ru>
2026-03-11 18:53:09 +0400

    Add JSON file, read and parsing file

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,20 @@
 import argparse
+import json
+
+
+def read_file(filepath):
+    with open(filepath, 'r') as file:
+        return json.load(file)
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    return f"Comparing {file_path1} and {file_path2} in {format_name} format"
+    data1 = read_file(file_path1)
+    data2 = read_file(file_path2)
+
+    return (f"File 1 ({file_path1}): {len(data1)} keys\n"
+            f"File 2 ({file_path2}): {len(data2)} keys\n"
+            f"Format: {format_name}\n"
+            f"Will compare {list(data1.keys())} with {list(data2.keys())}")
 
 
 def main():
@@ -11,18 +23,15 @@ def main():
         usage='gendiff [-h] [-f FORMAT] first_file second_file'
     )
 
-    parser.add_argument(
-        'first_file',
-    )
+    parser.add_argument('first_file')
 
-    parser.add_argument(
-        'second_file',
-    )
+    parser.add_argument('second_file')
 
     parser.add_argument(
         '-f', '--format',
         default='stylish',
-        help='set format of output',
+        metavar='FORMAT',
+        help='set format of output'
     )
 
     args = parser.parse_args()
