2f4dabcae99e7e96dd57c1e387eeb3769b91791d
Ivan63-coder <vanyaiudin@yandex.ru>
2026-03-11 23:10:50 +0400

    Add Comparing flat files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,5 @@
 import argparse
-import json
-
-
-def read_file(filepath):
-    with open(filepath, 'r') as file:
-        return json.load(file)
-
-
-def generate_diff(file_path1, file_path2, format_name='stylish'):
-    data1 = read_file(file_path1)
-    data2 = read_file(file_path2)
-
-    return (f"File 1 ({file_path1}): {len(data1)} keys\n"
-            f"File 2 ({file_path2}): {len(data2)} keys\n"
-            f"Format: {format_name}\n"
-            f"Will compare {list(data1.keys())} with {list(data2.keys())}")
+from gendiff import generate_diff
 
 
 def main():
@@ -24,20 +9,17 @@ def main():
     )
 
     parser.add_argument('first_file')
-
     parser.add_argument('second_file')
-
     parser.add_argument(
         '-f', '--format',
         default='stylish',
-        metavar='FORMAT',
         help='set format of output'
     )
 
     args = parser.parse_args()
 
-    result = generate_diff(args.first_file, args.second_file, args.format)
-    print(result)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
+    print(diff)
 
 
 if __name__ == '__main__':
